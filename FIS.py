371830563942c3527056c4ee8befbe7bcbee24ea
Gundas3073 <Gundas3073@gmail.com>
2015-03-29 17:38:33 -0400

    Create FIS.py

---
+++ FIS.py
@@ -0,0 +1,29 @@
+import urllib2
+
+def main():
+	global count
+	count = 0	
+	for n in range(5500,6000):    
+		global test 
+		test = n		
+		download_file("http://www.freeinfosociety.com/media/pdf/" + str(n) + ".pdf")
+	print str(count)
+
+def download_file(download_url):
+	try:
+		# Used to make sure no errors w/ HTTP request (AKA 404 Error trolls)		
+		response = urllib2.urlopen(download_url)
+		file = open(str(test) + ".pdf", 'wb')
+		file.write(response.read())
+		file.close()
+		print("Completed")
+		count = count + 1
+	except urllib2.HTTPError, e:
+		print "file number " + str(test) + " is not present"
+	except urllib2.URLError, e:
+		print "file number " + str(test) + " is not present"
+	except:
+		print " "
+	
+if __name__ == "__main__":
+    main()
